a60093203ccbe4139dfd5300940ab7c36fa959db
Artur Rychlewicz <8393226+artuross@users.noreply.github.com>
2020-09-18 22:22:20 +0200

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,16 @@
-import React, { Fragment } from 'react';
-import { Header } from './components/Header';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 export const App = () => {
 	return (
-		<Fragment>
-			<Header />
-
-			<p>Hello world!</p>
-
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-		</Fragment>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path={'/'} component={Home} />
+				{/*<Route path={'/search/:search'} component={Search} />*/}
+				{/*<Route path={'/user/:user'} component={User} />*/}
+				<Redirect to={'/'} />
+			</Switch>
+		</BrowserRouter>
 	);
 };
---
+++ src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import React, { Fragment } from 'react';
+import { Header } from '../components/Header';
+
+export const Home = () => {
+	return (
+		<Fragment>
+			<Header />
+
+			<p>Hello world!</p>
+
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+		</Fragment>
+	);
+};
